c9079b8be84a10cceb7d7e8c5d95cdcb2d030d95
FRANCISCO WALA NORONHA MACEDO <25203678@dsn127m1246728.cfp127aluno.local>
2026-09-15 16:09:00 -0300

    Teste

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
 
@@ -20,9 +24,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComponentesEstadoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    BasicComponentsScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +35,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BasicComponentsScreen(modifier: Modifier = Modifier){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComponentesEstadoTheme {
-        Greeting("Android")
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(text = "Aulas android")
+        Text(text = "com JetpackCompose")
     }
+
 }
