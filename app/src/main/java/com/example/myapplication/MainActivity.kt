c8e1323e7a97b85b1b0552e2547bdc5d15b6a613
FRANCISCO WALA NORONHA MACEDO <25203678@dsn127m1246728.cfp127aluno.local>
2026-09-15 15:34:56 -0300

    iniciando em android Studio com Kotlin

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() //De borda a borda
+        setContent {
+            MyApplicationTheme {
+                //definirconteudo
+                //Andaime = Scaffold
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LayoutScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(Color.Green)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                .background(Color.White)
+                .weight(1f)
+                .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text("Texto 1")
+                Text("Texto 2")
+                Text("Texto 3")
+            }
+            Row(
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .weight(1f)
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text("Texto 4")
+                Text("Texto 5")
+                Text("Texto 6")
+            }
+
+        }
+    }
